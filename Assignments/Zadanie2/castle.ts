1b852a008252e31f4db02cfde8bd987d543221d1
jfedak <jakubfedak1030@gmail.com>
2026-04-06 20:40:48 +0200

    feat: add windows to castle

--- Assignments/Zadanie2/castle.ts
+++ Assignments/Zadanie2/castle.ts
@@ -1,12 +1,16 @@
+const CORRIDOR_WIDTH = 5
+const CASTLE_SIZE = 10
+const CASTLE_HEIGHT = 8
+
 player.onChat("castle", function() {
-    const CORRIDOR_WIDTH = 5
-    const CASTLE_SIZE = 5
-    const CASTLE_HEIGHT = 5
     makeFloor(CASTLE_SIZE + CORRIDOR_WIDTH)
     makeCeiling(CASTLE_SIZE, CORRIDOR_WIDTH, CASTLE_HEIGHT)
     makeWalls(CASTLE_SIZE, CASTLE_HEIGHT, false)
     makeWalls(CASTLE_SIZE + CORRIDOR_WIDTH, CASTLE_HEIGHT, true)
     makeStairs(CASTLE_SIZE, CASTLE_HEIGHT)
+    makeWindows()
+    makeYard()
+    player.say("castle is finished")
 })
 
 function makeWallsRules(castleSize: number) {
@@ -66,5 +70,99 @@ function makeStairs(castleSize: number, castleHeight: number) {
         blocks.place(stair, pos(x, h, y))
         y = y-1
         h = h-1
+    }
+}
+
+function makeWindows() {
+    let holes = [
+        // front and back
+        [pos(-7, 2, 15), pos(-5, 5, 15)],
+        [pos(-1, 2, 15), pos(1, 5, 15)],
+        [pos(5, 2, 15), pos(7, 5, 15)],
+        [pos(-7, 2, -15), pos(-5, 5, -15)],
+        [pos(-1, 2, -15), pos(1, 5, -15)],
+        [pos(5, 2, -15), pos(7, 5, -15)],
+        // sides
+        [pos(15, 2, -7), pos(15, 5, -5)],
+        [pos(15, 2, -1), pos(15, 5, 1)],
+        [pos(15, 2, 5), pos(15, 5, 7)],
+        [pos(-15, 2, -7), pos(-15, 5, -5)],
+        [pos(-15, 2, -1), pos(-15, 5, 1)],
+        [pos(-15, 2, 5), pos(-15, 5, 7)],
+    ]
+
+    let wall = blocks.blockWithData(Block.CobblestoneWall, 7)
+
+    
+    for (let hole of holes) {
+        let [start, end] = hole
+        let x = (start.getValue(0) + end.getValue(0)) / 2
+        let z = (start.getValue(2) + end.getValue(2)) / 2
+
+
+        blocks.fill(Block.Air, start, end)
+        blocks.fill(wall, pos(x, 2, z), pos(x, 4, z))
+        blocks.place(Block.StoneBricks, pos(x, 5, z))
+
+        let side = (start.getValue(0) == end.getValue(0))
+        let stairs
+
+        if (side) {
+            stairs = [
+                blocks.blockWithData(Block.StoneBrickStairs, 7),
+                blocks.blockWithData(Block.StoneBrickStairs, 6)
+            ]
+        } else {
+            stairs = [
+                blocks.blockWithData(Block.StoneBrickStairs, 5),
+                blocks.blockWithData(Block.StoneBrickStairs, 4)
+            ]
+        }
+
+        blocks.place(stairs[0], start.add(pos(0, 3, 0)))
+        blocks.place(stairs[1], end)
+    }
+}
+
+function makeYard() {
+    let holes = [
+        [pos(-10, 2, -8), pos(-10, 4, 8)],
+        [pos(10, 2, -8), pos(10, 4, 8)],
+        [pos(-8, 2, -10), pos(8, 4, -10)],
+        [pos(-8, 2, 10), pos(8, 4, 10)],
+    ];
+
+    let pillarCoords = []
+    for (let hole of holes) {
+        let [start, end] = hole
+        blocks.fill(Block.Air, start, end)
+
+        let side = (start.getValue(0) == end.getValue(0))
+        let coords
+
+        if (side) {
+            let x = start.getValue(0)
+            pillarCoords.push([x, -6])
+            pillarCoords.push([x, -3])
+            pillarCoords.push([x, 3])
+            pillarCoords.push([x, 6])
+        } else {
+            let z = start.getValue(2)
+            pillarCoords.push([-6, z])
+            pillarCoords.push([-3, z])
+            pillarCoords.push([3, z])
+            pillarCoords.push([6, z])
+        }
+    }
+
+    blocks.fill(Block.Air, pos(-1, 0, 10), pos(1, 4, 10))
+    blocks.fill(Block.Air, pos(-1, 0, -10), pos(1, 4, -10))
+    blocks.fill(Block.Air, pos(10, 0, -1), pos(10, 4, 1))
+    blocks.fill(Block.Air, pos(-10, 0, -1), pos(-10, 4, 1))
+
+    let pillarBlock = blocks.blockWithData(Block.CobblestoneWall, 7)
+    for (let pillar of pillarCoords) {
+        let [x, z] = pillar
+        blocks.fill(pillarBlock, pos(x, 2, z), pos(x, 4, z))
     }
 }
